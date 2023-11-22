228bca67e910918caf520f31526b42c0a9b33a20
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-22 18:58:35 +0530

    Update MavenWrapperDownloader.java

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import java.net.*;
+/* import java.net.*;
 import java.io.*;
 import java.nio.channels.*;
 import java.util.Properties;
@@ -115,3 +115,4 @@ public class MavenWrapperDownloader {
     }
 
 }
+*/
