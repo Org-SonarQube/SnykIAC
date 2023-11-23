947e55286d3b32c23b541f7cf375caacff9cd960
Uma Narayanan <100426477+cunarayanan@users.noreply.github.com>
2023-11-23 21:08:04 +0530

    Update InsecureController.java

--- src/main/java/io/snyk/examples/snykest/InsecureController.java
+++ src/main/java/io/snyk/examples/snykest/InsecureController.java
@@ -1,9 +1,12 @@
+/*
 package org.springframework.security.samples.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+*/
 
 /**
  * @author Joe Grandja
